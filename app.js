759247bc6f5de91896eb47867e365c4446171d27
nickmly <nickmly@gmail.com>
2017-11-02 17:35:35 -0400

    Added post route for new campgrounds

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Salmon_Creek.jpg/1200px-Salmon_Creek.jpg"},
+    {name: "Mountains", image: "https://www.nps.gov/lacl/planyourvisit/images/Image-w-cred-cap_-1200w-_-Visit-Silver-Salmon-Creek-Page_2.jpg"}
+];
 
 ////////////////////////////////
 // ROUTES
@@ -12,13 +17,23 @@ app.get("/", function(req,res){
     res.render("landing");
 });
 
-app.get("/campgrounds", function(req,res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Salmon_Creek.jpg/1200px-Salmon_Creek.jpg"},
-        {name: "Mountains", image: "https://www.nps.gov/lacl/planyourvisit/images/Image-w-cred-cap_-1200w-_-Visit-Silver-Salmon-Creek-Page_2.jpg"}
-    ];
-
+app.get("/campgrounds", function(req,res){   
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req,res){
+    res.render("new.ejs");
+});
+
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var img = req.body.image;
+    campgrounds.push({
+        name: name,
+        image: img
+    });
+    res.redirect("/campgrounds");
 });
 
 
