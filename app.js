b66626c88d3360e0f333e0f75311b2ff3865a588
nickmly <nickmly@gmail.com>
2017-11-07 12:02:17 -0500

    More styling

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ app.set("view engine", "ejs");
 
 var campgrounds = [
     {name: "Salmon Creek", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Salmon_Creek.jpg/1200px-Salmon_Creek.jpg"},
-    {name: "Mountains", image: "https://www.nps.gov/lacl/planyourvisit/images/Image-w-cred-cap_-1200w-_-Visit-Silver-Salmon-Creek-Page_2.jpg"}
+    {name: "Mountains", image: "https://www.nps.gov/lacl/planyourvisit/images/Image-w-cred-cap_-1200w-_-Visit-Silver-Salmon-Creek-Page_2.jpg"},
+    {name: "Colorado", image: "http://www.campoutcolorado.com/wp-content/grand-media/image/camp-out-colorado-carter-lake-pine-campground.jpg"},
+    {name: "Salmon Creek", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Salmon_Creek.jpg/1200px-Salmon_Creek.jpg"},
+    {name: "Mountains", image: "https://www.nps.gov/lacl/planyourvisit/images/Image-w-cred-cap_-1200w-_-Visit-Silver-Salmon-Creek-Page_2.jpg"},
+    {name: "Colorado", image: "http://www.campoutcolorado.com/wp-content/grand-media/image/camp-out-colorado-carter-lake-pine-campground.jpg"}
 ];
 
 ////////////////////////////////
