3ede337c8a1b72162b3aea99268678801dbfd18c
nickmly <nickmly@gmail.com>
2017-11-13 20:44:47 -0500

    Logout functionality

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ app.set("view engine", "ejs");
 // Seed the database (seeds.js)
 seedDB();
 
+// Set user data to be sent through every route
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
 ////////////////////////////////
 // ROUTES
 ////////////////////////////////
@@ -62,18 +67,18 @@ app.get("/campgrounds", function (req, res) {
         if (err) {
             console.log(err);
         } else {
-            res.render("campground/index", { campgrounds: campgrounds });
+            res.render("campground/index", { campgrounds: campgrounds});
         }
     });
 });
 
 // NEW
-app.get("/campgrounds/new", function (req, res) {
+app.get("/campgrounds/new", isLoggedIn, function (req, res) {
     res.render("campground/new");
 });
 
 // CREATE
-app.post("/campgrounds", function (req, res) {
+app.post("/campgrounds", isLoggedIn, function (req, res) {
     var name = req.body.name;
     var img = req.body.image;
     var desc = req.body.desc;
@@ -109,7 +114,7 @@ app.get("/campgrounds/:id", function (req, res) {
 ////////////////////////////////
 
 // COMMENT NEW
-app.get("/campgrounds/:id/comments/new", function (req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, foundCamp) {
         if (err) {
             console.log(err);
@@ -120,7 +125,7 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
 });
 
 // COMMENT CREATE
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function (req, res) {
     Campground.findById(req.params.id, function (err, foundCamp) {
         if (err) {
             console.log(err);
@@ -162,8 +167,38 @@ app.post("/register", function (req, res) {
 
     });
 });
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+// Login with passport authentication
+// If user entered correct info, go to campgrounds page
+// Otherwise go to login page again
+app.post("/login", passport.authenticate("local", 
+{
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req,res){
+    
+});
+
+app.get("/logout", function(req,res){
+    req.logout();// Built in passport function for logging out
+    res.redirect("/campgrounds");
+});
 ////////////////////////////////
 ////////////////////////////////
+
+// Middleware function to check if user is logged in
+// Next is the function that will run if user is logged in
+// If not, redirect to login page
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 // Server listening on port 3000 for routes
 app.listen(3000, process.env.IP, function () {
