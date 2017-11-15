4ea8c945a792aa10993c8320041f7472e6002154
nickmly <nickmly@gmail.com>
2017-11-14 23:13:50 -0500

    Editing comments

--- routes/comments.js
+++ routes/comments.js
@@ -33,6 +33,7 @@ router.post("/", isLoggedIn, function (req, res) {
                     // add username and id to commment
                     comment.author.id = req.user._id;
                     comment.author.username = req.user.username;
+                    comment.campground = foundCamp;
 
                     // save comment
                     comment.save();
@@ -49,6 +50,56 @@ router.post("/", isLoggedIn, function (req, res) {
         }
     });
 });
+
+// COMMENT EDIT
+router.get("/:comment_id/edit", checkOwner, function(req,res){
+    Comment.findById(req.params.comment_id, function(err, comment){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        } else {  
+            res.render("comment/edit", {comment: comment, campground_id: req.params.id});
+        }
+    });
+});
+
+// COMMENT UPDATE
+router.put("/:comment_id", checkOwner, function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// COMMENT DELETE
+router.delete("/:comment_id", checkOwner, function(req,res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err, comment){
+        res.redirect("/campgrounds/" + req.params.id);
+    })
+});
+
+function checkOwner(req,res,next){
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, comment){
+            if(err) {
+                console.log(err);
+                res.redirect("back");
+            } else {
+                if(comment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("/login");
+    }
+}
 
 ////////////////////////////////
 ////////////////////////////////
